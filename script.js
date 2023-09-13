b59f313ba326012e63591eb696be38feb0fb2953
Navi9519 <dedicivanovic@gmail.com>
2023-09-13 17:52:08 +0200

    added variables

---
+++ script.js
@@ -0,0 +1,9 @@
+var integer = 10;
+var decimal = 0.5;
+var string = "Ivan";
+var boolean = false;
+
+console.log(typeof integer);
+console.log(typeof decimal);
+console.log(typeof string);
+console.log(typeof boolean);
