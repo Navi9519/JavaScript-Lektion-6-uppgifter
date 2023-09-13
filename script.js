839c661e8ec824b489dc1cc16fd7ae416a71e823
Navi9519 <dedicivanovic@gmail.com>
2023-09-13 18:42:19 +0200

    uppgifter + challenge 1

--- script.js
+++ script.js
@@ -7,3 +7,24 @@ console.log(typeof integer);
 console.log(typeof decimal);
 console.log(typeof string);
 console.log(typeof boolean);
+
+var godis = ["bilar", "kexchoklad", "marabou", "daim"];
+console.log(godis);
+console.log(godis[3]);
+console.log(godis[godis.length - 1]);
+
+function marabouIsBest() {
+  alert(godis[2] + " Is the best candy");
+}
+
+document.querySelector(".myBtn").addEventListener("click", marabouIsBest);
+
+function showUserName() {
+  var name = prompt("Write me your name");
+
+  document.querySelector(".text").innerHTML = "ditt namn är " + name;
+
+  console.log(name);
+}
+
+document.querySelector(".nameBtn").addEventListener("click", showUserName);
